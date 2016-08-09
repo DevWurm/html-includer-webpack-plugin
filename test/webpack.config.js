9691f64355df22890154456435c1c9a02e84faa8
DevWurm <devwurm@devwurm.net>
2016-08-10 01:34:32 +0200

    Adding test setup
    Manual Webpack test setup

---
+++ test/webpack.config.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlIncluderWebpackPlugin = require('../lib').default;
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new HtmlIncluderWebpackPlugin()
+    ]
+};
