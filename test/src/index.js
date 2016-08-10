3a0b66109ee86d75143bd4baeb6bd2b35cb2d17b
DevWurm <devwurm@devwurm.net>
2016-08-10 02:44:17 +0200

    Adding test setup for tag functionallity

--- test/src/index.js
+++ test/src/index.js
@@ -1,3 +1,11 @@
-console.log(require('../../lib/include-loader!./test.html'));
-console.log(require('../../lib/include-loader!./test.html'));
+// test if file is included correctly
+require('../../lib/include-loader!./test.html');
+
+// test if file is not included twice
+require('../../lib/include-loader!./test.html');
+
+// test if file is even not included twice, when required in other file
 require('./test/test');
+
+// test if file content is wrapped in specified tag (script)
+require('../../lib/include-loader?script!./foo.js')
