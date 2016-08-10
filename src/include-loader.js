a2b86055eb941d3fa4c07a079f108e6f562b53ef
DevWurm <devwurm@devwurm.net>
2016-08-10 02:41:19 +0200

    Adding tag functionallity
    * a custom tag name can be specified via the query parameter of the loader
    * the content of the loaded file will be wrapped inside of this tag type
    
    Changing return value of loader
    
    * loader now returns an empty string to reduce the size of the bundled
    files

--- src/include-loader.js
+++ src/include-loader.js
@@ -1,6 +1,12 @@
 module.exports = function includeLoader(source) {
   if (this._compiler.htmlIncluder) {
-    this._compiler.htmlIncluder.addChunk(source);
+    if (this.query && this.query.charAt(0) == '?') {
+      const tag = this.query.substring(1);
+      this._compiler.htmlIncluder.addChunk(`<${tag}>${source}</${tag}>`)
+    } else {
+      this._compiler.htmlIncluder.addChunk(source);
+    }
   }
-  return `"${source}"`;
+
+  return '';
 }
